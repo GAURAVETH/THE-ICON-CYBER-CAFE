13a9613ba188b96b7a47f2b8d5f7c4148e3eb459
Gaurav Kumar <gauravkreth@gmail.com>
2026-05-21 22:43:53 +0530

    fixed for the server deployment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ dotenv.config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "THE ICON CYBER CAFE API Running"
+    });
+});
+
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
